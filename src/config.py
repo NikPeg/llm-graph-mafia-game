70fd07c29e70f92ff13ac690790b6c1dc9522cb7
NikPeg <peganov.nik@gmail.com>
2025-06-25 22:34:55 +0300

    only_free

--- src/config.py
+++ src/config.py
@@ -19,29 +19,8 @@ FIREBASE_CREDENTIALS_PATH = (
 
 # Game settings
 
-CLAUDE_3_7_SONNET = "anthropic/claude-3.7-sonnet"
-MODELS = [
-    "google/gemini-2.0-flash-001",
-    "google/gemini-flash-1.5",
-    "google/gemini-flash-1.5-8b",
-    "openai/gpt-4o-mini",
-    "meta-llama/llama-3.3-70b-instruct",
-    "google/gemini-2.0-flash-lite-001",
-    "meta-llama/llama-3.1-70b-instruct",
-    # "deepseek/deepseek-r1", # takes too long to respond
-    "gryphe/mythomax-l2-13b",
-    "microsoft/wizardlm-2-8x22b",
-    "mistralai/mistral-small-24b-instruct-2501",
-    "nousresearch/hermes-3-llama-3.1-405b",
-    "minimax/minimax-01",
-    # "sao10k/l3-euryale-70b", # max context 8192
-    "deepseek/deepseek-chat",
-    "openai/gpt-4o",
-    "anthropic/claude-3.5-sonnet",
-    "deepseek/deepseek-r1-distill-llama-70b",
-    CLAUDE_3_7_SONNET,
-    CLAUDE_3_7_SONNET + ":thinking",
-]
+CLAUDE_3_7_SONNET = "deepseek/deepseek-r1-distill-llama-70b:free"
+MODELS = []
 
 FREE_MODELS = [
     "deepseek/deepseek-r1:free",
